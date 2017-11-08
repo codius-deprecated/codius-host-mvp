7d29ff6b0a10740b1fa003dd8c99e6f7c697ae80
Stefan Thomas <justmoon@members.fsf.org>
2017-11-07 16:15:52 -0800

    feat: proxy requests to the contract by hash

--- src/lib/app.js
+++ src/lib/app.js
@@ -3,18 +3,20 @@
 const fs = require('fs-extra')
 const debug = require('debug')('codius-host')
 const path = require('path')
-const crypto = require('crypto')
+const moment = require('moment')
 const Koa = require('koa')
 const Router = require('koa-router')
 const Logger = require('koa-logger')
 const BodyParser = require('koa-bodyparser')
 const Ilp = require('koa-ilp')
 const Boom = require('boom')
-const canonicalJson = require('canonical-json')
-const getPort = require('get-port')
 const ILP = require('ilp')
 
 const Config = require('./config')
+const Contracts = require('./contracts')
+const Proxy = require('./proxy')
+
+const { hashManifest } = require('../util/manifest')
 
 const dollarsPerMonth = process.env.CODIUS_DOLLARS_PER_MONTH || 5
 const dollarsPerXrp = 0.2
@@ -24,15 +26,13 @@ const dropsPerMonth = xrpPerMonth * dropsPerXrp
 const monthsPerSecond = 0.0000003802571
 const dropsPerSecond = dropsPerMonth * monthsPerSecond
 
-const { spawnSync } = require('child_process')
-
 class App {
   constructor (deps) {
     this.config = deps(Config)
     this.koa = deps(Koa)
     this.router = deps(Router)
-
-    this.contracts = {}
+    this.proxy = deps(Proxy)
+    this.contracts = deps(Contracts)
 
     const { ilpPlugin, ilpCredentials } = this.config
 
@@ -44,9 +44,10 @@ class App {
   listen () {
     const app = this.koa
     const router = this.router
-    const { port, hostId } = this.config
+    const { port } = this.config
 
     app.use(Logger())
+    app.use(this.proxy.getMiddleware())
     app.use(BodyParser())
     app.use(router.routes())
     app.use(router.allowedMethods({
@@ -54,11 +55,6 @@ class App {
       notImplemented: () => Boom.notImplemented(),
       methodNotAllowed: () => Boom.methodNotAllowed()
     }))
-
-    const SHA256_REGEX = /^[0-9a-fA-F]{64}$/
-
-    const digestToPath = (digest) =>
-      path.resolve(__dirname, 'data', digest.substring(0, 2), digest)
 
     const price = (ctx) => {
       const duration = ctx.query.duration || 3600
@@ -68,70 +64,31 @@ class App {
     router.options('/start', this.ilp.options({ price }))
     router.post('/start', this.ilp.paid({ price }), async (ctx) => {
       const { manifest } = ctx.request.body
-      const { image, environment, port } = manifest
-      const command = manifest.command || []
       const duration = ctx.query.duration || 3600
       const durationMs = duration * 1000
 
-      const manifestHash = crypto.createHash('sha256').update(canonicalJson(manifest)).digest('hex')
+      const manifestHash = hashManifest(manifest)
 
-      ctx.body = {
-        manifestHash
-      }
-
-      const deployed = this.contracts[manifestHash]
+      const deployed = !!this.contracts.get(manifestHash)
       if (deployed) {
-        clearTimeout(deployed.killTimeout)
-        deployed.killTime += durationMs
-        deployed.killTimeout = this.setKillTime(manifestHash, deployed.killTime)
+        this.contracts.increaseKillTime(manifestHash, durationMs)
 
+        debug('extended contract %s by %d seconds', manifestHash, duration)
         ctx.status = 200
-        return
+      } else {
+        await this.contracts.start(manifest, duration)
+
+        debug('started', manifestHash)
+        ctx.status = 201
       }
 
-      const hostPort = await getPort()
-
-      const environmentOpts = []
-      for (const key of Object.keys(environment || {})) {
-        environmentOpts.push('-e')
-        environmentOpts.push(`${key}=${environment[key]}`)
+      const expiry = this.contracts.getExpiry(manifestHash)
+      ctx.body = {
+        manifestHash,
+        expiry: expiry.toISOString(),
+        expiryHuman: moment(expiry).fromNow(),
+        url: this.contracts.getUrl(manifestHash)
       }
-
-      {
-        const { status } = spawnSync('docker', [ 'pull', image ], { stdio: 'inherit' })
-
-        if (status) {
-          throw new Error('Docker pull failed with status ' + status)
-        }
-      }
-
-      {
-        const { status } = spawnSync('docker', [
-          'run', '-d', '--rm',
-          '-p', `${hostPort}:${port}`,
-          '--name', manifestHash.substring(0, 16),
-          '--label', 'codius=' + hostId,
-          ...environmentOpts,
-          image,
-          ...command
-        ], { stdio: 'inherit' })
-
-        if (status) {
-          throw new Error('Docker run failed with status ' + status)
-        }
-      }
-
-      const killTime = Date.now() + durationMs
-      const killTimeout = this.setKillTime(manifestHash, killTime)
-
-      this.contracts[manifestHash] = {
-        manifest,
-        killTime,
-        killTimeout
-      }
-
-      debug('started', manifestHash)
-      ctx.status = 201
     })
 
     router.get('/', (ctx) => {
@@ -141,16 +98,6 @@ class App {
     app.listen(port)
 
     console.log(`Codius listening on port ${port}`)
-  }
-
-  setKillTime (manifestHash, killTime) {
-    return setTimeout(() => {
-      spawnSync('docker', [
-        'stop', '-t', '10',
-        manifestHash.substring(0, 16)
-      ], { stdio: 'inherit' })
-      delete this.contracts[manifestHash]
-    }, killTime - Date.now())
   }
 }
 
---
+++ src/lib/contracts.js
@@ -0,0 +1,117 @@
+'use strict'
+
+const getPort = require('get-port')
+const { spawnSync } = require('child_process')
+
+const Config = require('./config')
+
+const { hashManifest } = require('../util/manifest')
+const { hashToLabel } = require('../util/base32')
+
+class Contracts {
+  constructor (deps) {
+    this.config = deps(Config)
+
+    this._contracts = {}
+  }
+
+  async start (manifest, duration) {
+    const manifestHash = hashManifest(manifest)
+    const { hostId } = this.config
+    const { image, environment, port } = manifest
+    const command = manifest.command || []
+    const durationMs = duration * 1000
+
+    const hostPort = await getPort()
+
+    const environmentOpts = []
+    for (const key of Object.keys(environment || {})) {
+      environmentOpts.push('-e')
+      environmentOpts.push(`${key}=${environment[key]}`)
+    }
+
+    {
+      const { status } = spawnSync('docker', [ 'pull', image ], { stdio: 'inherit' })
+
+      if (status) {
+        throw new Error('Docker pull failed with status ' + status)
+      }
+    }
+
+    {
+      const { status } = spawnSync('docker', [
+        'run', '-d', '--rm',
+        '-p', `${hostPort}:${port}`,
+        '--name', manifestHash.substring(0, 16),
+        '--label', 'codius=' + hostId,
+        ...environmentOpts,
+        image,
+        ...command
+      ], { stdio: 'inherit' })
+
+      if (status) {
+        throw new Error('Docker run failed with status ' + status)
+      }
+    }
+
+    const killTime = Date.now() + durationMs
+    this._contracts[manifestHash] = {
+      manifest,
+      killTime,
+      killTimeout: this._createKillTimeout(manifestHash, killTime),
+      hostPort
+    }
+  }
+
+  stop (manifestHash) {
+    spawnSync('docker', [
+      'stop', '-t', '10',
+      manifestHash.substring(0, 16)
+    ], { stdio: 'inherit' })
+    delete this._contracts[manifestHash]
+  }
+
+  _createKillTimeout (manifestHash, killTime) {
+    return setTimeout(() => {
+      this.stop(manifestHash)
+    }, killTime - Date.now())
+  }
+
+  setKillTime (manifestHash, killTime) {
+    const contract = this._contracts[manifestHash]
+
+    if (contract.killTimeout) {
+      clearTimeout(contract.killTimeout)
+    }
+
+    contract.killTime = killTime
+    contract.killTimeout = this._createKillTimeout(manifestHash, killTime)
+  }
+
+  increaseKillTime (manifestHash, killTimeIncrease) {
+    this.setKillTime(manifestHash, this.get(manifestHash).killTime + killTimeIncrease)
+  }
+
+  get (manifestHash) {
+    return this._contracts[manifestHash]
+  }
+
+  getExpiry (manifestHash) {
+    return new Date(this._contracts[manifestHash].killTime)
+  }
+
+  getUrl (manifestHash) {
+    const { hostname, port } = this.config
+    return 'http://' +
+      hashToLabel(manifestHash) + '.' + hostname +
+      (port ? ':' + port : '') +
+      '/'
+  }
+
+  getInternalUrl (manifestHash) {
+    const { hostPort } = this.get(manifestHash)
+    return 'http://localhost:' + hostPort
+  }
+}
+
+module.exports = Contracts
